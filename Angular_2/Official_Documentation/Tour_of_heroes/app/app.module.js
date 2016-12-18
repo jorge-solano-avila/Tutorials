0dbc004e35bf0ce1d4a6d786ab960e7dcfd976d6
jorge52an <jorgesolavila@gmail.com>
2016-12-18 14:36:06 -0500

    chore: change hero to hero class

--- Angular_2/Official_Documentation/Tour_of_heroes/app/app.module.js
+++ Angular_2/Official_Documentation/Tour_of_heroes/app/app.module.js
@@ -8,9 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var app_component_1 = require('./app.component');
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var app_component_1 = require("./app.component");
 var AppModule = (function () {
     function AppModule() {
     }
